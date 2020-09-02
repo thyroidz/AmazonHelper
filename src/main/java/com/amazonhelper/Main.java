0caa38c5d8e56e691d0caa083f0eda79a48a6f8e
Mike Free <mfree@clearwateranalytics.com>
2020-09-02 14:24:31 -0600

    using standard maven stucture. removing .idea directory. using maven-assembly-plugin to make jar-with-dependencies

--- src/main/java/com/amazonhelper/Main.java
+++ src/main/java/com/amazonhelper/Main.java
@@ -1,4 +1,5 @@
-package com;
+package com.amazonhelper;
+
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFCell;
---
+++ src/test/java/com/amazonhelper/MainTest.java
@@ -0,0 +1,10 @@
+package com.amazonhelper;
+
+
+public class MainTest
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello test");
+    }
+}
