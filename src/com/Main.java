6ed3e7b3071df3275d0402fd6dfb58efdca3a248
Alex Miller <chocolatemuffintoast@gmail.com>
2020-09-01 20:49:12 -0600

    Ignore files and Maven
    Set up maven

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,10 +1,21 @@
 package com;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class Main
 {
-
     public static void main(String[] args)
     {
+        XSSFWorkbook wb = new XSSFWorkbook();
+        XSSFSheet sheet = wb.createSheet();
+        Row row = sheet.createRow(0);
+        Cell cell = row.createCell(0);
+
+
         System.out.println("Hello world");
     }
 }
